999356b360b5fb7181b5042dbcf9487af4ed58bb
shrelhan <shrelhan.relhan@gmail.com>
2024-08-09 20:45:02 +0530

    new update

--- script.js
+++ script.js
@@ -16,14 +16,53 @@ window.addEventListener("scroll", () => {
       "bg-white",
       "bg-opacity-50",
       "backdrop-blur-lg",
-      "shadow-sm"
+      "shadow-sm",
+      "dark:bg-darkTheme",
+      "dark:shadow-white/20"
+    );
+    navLinks.classList.remove(
+      "bg-white",
+      "shadow-sm",
+      "bg-opacity-50",
+      "dark:border",
+      "dark:border-white/50",
+      "dark:bg-transparent"
     );
   } else {
     navBar.classList.remove(
       "bg-white",
       "bg-opacity-50",
       "backdrop-blur-lg",
-      "shadow-sm"
+      "shadow-sm",
+      "dark:bg-darkTheme",
+      "dark:shadow-white/20"
+    );
+    navLinks.classList.add(
+      "bg-white",
+      "shadow-sm",
+      "bg-opacity-50",
+      "dark:border",
+      "dark:border-white/50",
+      "dark:bg-transparent"
     );
   }
 });
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
+}
